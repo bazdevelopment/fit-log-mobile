60ae34b51c57142a2ab8ffacd46c662c8a24428e
Baziluc Marian <bazilucmarian98@yahoo.com>
2024-04-16 09:45:48 +0300

    filto-mobile-68-adressed comments

--- src/screens/onboarding-second/success-onboarding-screen/index.tsx
+++ src/screens/onboarding-second/success-onboarding-screen/index.tsx
@@ -9,24 +9,22 @@ import { ISuccessOnboardingScreen } from "./success-onboarding-screen.interface"
  */
 export default function SuccessOnboardingScreen({ onFinish }: ISuccessOnboardingScreen) {
   return (
-    <>
-      <View className="flex-1 flex-col items-center justify-start">
-        <CongratsIllustration width={350} height={300} />
-        <Label
-          labelText="Congratulations! 🏆"
-          as="h1"
-          additionalLabelStyle="font-inter-semi-bold text-primary-default text-center p-4"
-        />
+    <View className="flex-1 flex-col items-center justify-start">
+      <CongratsIllustration width={350} height={300} />
+      <Label
+        labelText="Congratulations! 🏆"
+        as="h1"
+        additionalLabelStyle="font-inter-semi-bold text-primary-default text-center p-4"
+      />
 
-        <Label
-          labelText="Tap the button below kickstart your transformation"
-          as="h5"
-          additionalLabelStyle="text-gray-500 px-8 text-center"
-        />
-        <View className="top-8 w-1/2">
-          <Button buttonText="Get started! 💪" onPress={onFinish} variant="primary" />
-        </View>
+      <Label
+        labelText="Tap the button below kickstart your transformation"
+        as="h5"
+        additionalLabelStyle="text-gray-500 px-8 text-center"
+      />
+      <View className="top-8 w-1/2">
+        <Button buttonText="Get started! 💪" onPress={onFinish} variant="primary" />
       </View>
-    </>
+    </View>
   );
 }
