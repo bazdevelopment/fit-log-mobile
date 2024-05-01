f0a3bfcaa7443a0ac5eb7c4f7a73b7480179f9d2
Baziluc Marian <bazilucmarian98@yahoo.com>
2024-05-02 00:15:35 +0300

    fitlog-96-removed useRef

--- src/hooks/use-segmented-selection/use-segmented-selection.ts
+++ src/hooks/use-segmented-selection/use-segmented-selection.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 import { ISegmentedControlOption } from "../../components/organisms/segmented-control/SegmentedControl.interface";
 
